7c342a5c97173d97534dd1a81a1944d2822a4cf0
Laura TAMIC <tamiclaura@gmail.com>
2023-11-21 11:20:52 +0100

    réglage des problèmes

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,12 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  devise_for :users
+
   root to: "pages#home"
   # Defines the root path route ("/")
   # root "posts#index"
   resources :destinations, only: %i[index show new create] do
-    resources :reservations
-  end
-  resources :users, only: %i[show new create] do
     resources :reservations
   end
   resources :reservations
