50b7a5e48ed4cbed2159e798fbc6f48ceab7d2f4
Laura TAMIC <tamiclaura@gmail.com>
2023-11-22 14:24:24 +0100

    stimulus

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,8 +1,15 @@
-// import { Controller } from "@hotwired/stimulus"
-// import flatpickr from "flatpickr"; // You need to import this to use new flatpickr()
+import { Controller } from "@hotwired/stimulus"
+import flatpickr from "flatpickr"; // You need to import this to use new flatpickr()
+// import { French } from "flatpickr/dist/l10n/fr.js"
 
-// export default class extends Controller {
-//   connect() {
-//     flatpickr(this.element)
-//   }
-// }
+
+export default class extends Controller {
+  connect() {
+    new flatpickr(this.element, {
+      mode: "range",
+      // "locale": French,
+      minDate: "today", //date.value en stimulus pour reprendre la date_of_availability
+      dateFormat: "d-m-y"
+    })
+  }
+}
